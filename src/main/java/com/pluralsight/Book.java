6ee7957d133a3d0000f9c5fcb3f72248b13ce125
Tatyionna Williams <byronwilliams055@gmail.com>
2024-10-08 09:00:37 -0500

    add library

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class Book {
+    public int idNumber;
+    public String isbn;
+    public String title;
+    public boolean isCheckedOut;
+    public String checkedOutTo;
+
+    public Book () {
+        this.idNumber = 00000;
+        this.isbn = "";
+        this.title = "";
+        this.isCheckedOut =
+        this.checkedOutTo = "";
+    }
+    checkOut (name)
+}
